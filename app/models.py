2c458f36577c81b71f29ca541a6dcc18ba5c5907
Bunlong Ly <bunlongly03@gmail.com>
2024-12-04 16:21:08 +0700

    update work_count

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import re
 
 ARTICLE_STATUS = (
     ("draft", "draft"),
@@ -15,7 +16,7 @@ class UserProfile(AbstractUser):
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True, default="")
-    word_count = models.IntegerField()
+    word_count = models.IntegerField(blank=True, default="")
     twitter_post = models.TextField(blank=True, default="")
     status = models.CharField(
         max_length=20,
@@ -24,3 +25,8 @@ class Article(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        text = re.sub(r"<[^>]*>", "", self.content).replace("&nbsp;", " ")
+        self.word_count = len(re.findall(r"\b\w+\b", text))
+        super().save(*args, **kwargs)
