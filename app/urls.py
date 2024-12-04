1493411f2e550269c27ff4c973178b7795a1b7ef
Bunlong Ly <bunlongly03@gmail.com>
2024-12-04 15:12:51 +0700

    cleaning up article urls

--- app/urls.py
+++ app/urls.py
@@ -10,11 +10,11 @@ urlpatterns = [
     # path("", home, name="home"),
     # path("articles/create/", ArticleCreateView.as_view(), name="create_article"),
     path("", ArticleListView.as_view(), name="home"),
-    path("articles/create/", ArticleCreateView.as_view(), name="create_article"),
+    path("create/", ArticleCreateView.as_view(), name="create_article"),
     path(
-        "articles/<int:pk>/update/", ArticleUpdateView.as_view(), name="update_article"
+        "<int:pk>/update/", ArticleUpdateView.as_view(), name="update_article"
     ),
     path(
-        "articles/<int:pk>/delete/", ArticleDeleteView.as_view(), name="delete_article"
+        "<int:pk>/delete/", ArticleDeleteView.as_view(), name="delete_article"
     ),
 ]
